6405dba575c7ee05670dc0a6bf95b83a55ecb11b
MaleHunter <peitong_cong@163.com>
2021-01-12 16:48:13 +0800

    品牌列表的条件查询功能实现，完成测试

--- qingcheng_interface/src/main/java/com/qingcheng/service/goods/BrandService.java
+++ qingcheng_interface/src/main/java/com/qingcheng/service/goods/BrandService.java
@@ -4,6 +4,7 @@ import com.qingcheng.entity.PageResult;
 import com.qingcheng.pojo.goods.Brand;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: MaleHunter
@@ -17,4 +18,5 @@ public interface BrandService {
 
   public List<Brand> findAll();
   public PageResult<Brand> findPage(int page, int size);
+  public List<Brand> findList(Map<String,Object> searchMap);
 }
--- qingcheng_service_goods/src/main/java/com/qingcheng/service/impl/BrandServiceImpl.java
+++ qingcheng_service_goods/src/main/java/com/qingcheng/service/impl/BrandServiceImpl.java
@@ -8,8 +8,10 @@ import com.qingcheng.entity.PageResult;
 import com.qingcheng.pojo.goods.Brand;
 import com.qingcheng.service.goods.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: MaleHunter
@@ -32,6 +34,27 @@ public class BrandServiceImpl implements BrandService {
     PageHelper.startPage(page, size);
     Page<Brand> pageResult = (Page<Brand>)brandMapper.selectAll();
     return new PageResult<>(pageResult.getTotal(),pageResult.getResult());
+  }
+
+  @Override
+  /**
+   * 条件查询
+   */
+  public List<Brand> findList(Map<String, Object> searchMap) {
+    Example example = new Example(Brand.class);
+    Example.Criteria criteria = example.createCriteria();
+    if (searchMap != null){
+      //模糊查询
+      if (searchMap.get("name") != null && !"".equals(searchMap.get("name"))){
+        criteria.andLike("name","%" + (String)searchMap.get("name") + "%");
+      }
+      //精确匹配
+      if (searchMap.get("letter") != null && !"".equals(searchMap.get("letter"))){
+        criteria.andEqualTo("letter",(String)searchMap.get("letter"));
+      }
+    }
+    return brandMapper.selectByExample(example);
+
   }
 
 }
